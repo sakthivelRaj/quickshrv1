7822f9b4069c355019942f4e00695390bd995ff3
Sakthivel <sakthi.51293@gmail.com>
2020-12-02 09:38:26 +0530

    heroku req

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn quickshare.wsgi --log-file-
--- quickshare/quickshare/settings.py
+++ quickshare/quickshare/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'rwbzc&awsze=jzrq5p8%w6v-^7+m98_kx6e)s+yy@jra9-@7*2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quicksharev1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -51,6 +51,9 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,7 +129,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
